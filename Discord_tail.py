47baaf78b185c075dedca585fecdbc872097a82a
Stuart Bildfell <bildfells760@gmail.com>
2017-05-26 22:36:21 -0600

    Updated emoji, expanded on HTTP exception handling

--- Discord_tail.py
+++ Discord_tail.py
@@ -69,39 +69,47 @@ def DontStarveReactionFilter(line):
     
     # Deerclops
     if "deerclop" in line:
-        return "<:deerclops:290486755226812416>"
+        return "<:deerclops:317613422252523521>"
     
     # Moose Goose
     elif "moose" in line or "goose" in line:
-        return "<:goose:290486755327475712>"
+        return "<:moose:317613438945853440>"
     
     # Mosling
     elif "mosling" in line:
-        return "<:mosling:290486757009522694>"
+        return "<:mosling:317613438618697728>"
     
     # Bearger
-    elif "bearger" in line:
-        return "<:bearger:290486754916696064>"
+    elif "bear" in line:
+        return "<:bearger:317613422084620300>"
     
     # Dragonfly
     elif "dragonfly" in line or "dfly" in line:
-        return "<:dragonfly:290486755319087104>"
+        return "<:dragonfly:317613422403387392>"
+    
+    # Spider Queen
+    elif "spider queen" in line:
+        return "<:spider_queen:317613590406234114>"
     
     # Ancient Guardian
     elif "guardian" in line:
-        return "<:guardian:290488044702662656>"
+        return "<:guardian:317613550052966400>"
     
     # Klaus
     elif "klaus" in line:
-        return "<:klaus:290486755495378944>"
+        return "<:klaus:317613506755166210>"
     
     # Bee Queen
     elif "bee queen" in line:
-        return "<:bee_queen:290486755088662538>"
+        return "<:bee_queen:317613507128328193>"
     
     # Toadstool
     elif "toad" in line:
-        return "<:toadstool:290486761065545728>"
+        return "<:toadstool:317613507552083968>"
+    
+    # Ancient Fuelweaver
+    elif "fuelweaver" in line:
+        return "<:fuelweaver:317619375366733824>"
     
     # No match found
     else:
@@ -135,8 +143,10 @@ async def file_tail(channelID, filename, time):
                     print("FORBIDDEN EXCEPTION (403): Bot doesn't have permissions to send message.")
                 except discord.NotFound:
                     print("NOT FOUND EXCEPTION (404): Couldn't find channel with ID \"{}\", message not sent.".format(channelID))
-                except discord.HTTPException:
+                except discord.HTTPException as e:
                     print("HTTP EXCEPTION: HTTP request failed, couldn't send message.")
+                    print("Response: \"{}\"".format(e.response))
+                    print("Text: \"{}\"".format(e.text))
                 except discord.InvalidArgument:
                     print("INVALID ARGUMENT EXCEPTION: Destination parameter invalid, couldn't send message.")
                 else:
@@ -151,8 +161,10 @@ async def file_tail(channelID, filename, time):
                         print("FORBIDDEN EXCEPTION (403): Bot doesn't have permissions to add reaction.")
                     except discord.NotFound:
                         print("NOT FOUND EXCEPTION (404): Couldn't find message or emoji, reaction not added.")
-                    except discord.HTTPException:
+                    except discord.HTTPException as e:
                         print("HTTP EXCEPTION: HTTP request failed, couldn't add reaction.")
+                        print("Response: \"{}\"".format(e.response))
+                        print("Text: \"{}\"".format(e.text))
                     except discord.InvalidArgument:
                         print("INVALID ARGUMENT EXCEPTION: Message or emoji parameter invalid, couldn't add reaction.")
         
