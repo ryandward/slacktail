98552a3d5e103b141325084b3e995c4d6722070a
Ryan Valverde de Ward <>
2022-05-07 09:29:33 -0500

    massive rewrite to work for everqueest

--- Discord_tail.py
+++ Discord_tail.py
@@ -9,107 +9,44 @@ Basis for Discord communication from https://github.com/Rapptz/discord.py/blob/a
 import argparse
 import os
 import discord
+from discord.ext import commands
 import asyncio
 import re
 import sys
+import datetime
+
 
 client = discord.Client()
 
 def DontStarvePrependEmoji(line):
     # Join
-    if re.match(r'\[(\d+):(\d+):(\d+)\]: \[Join Announcement\] (.+)$', line):
-        return "<:balloons:290486234956955648> <:balloons:290486234956955648> " + line
+    if "say" in line:
+        return ":speech_balloon:" + line
 
-    # Leave
-    elif re.match(r'\[(\d+):(\d+):(\d+)\]: \[Leave Announcement\] (.+)$', line):
-        return ":warning: :warning: " + line
+    elif "tells the guild" in line or "tell the guild" in line:
+        return ":speech_balloon:" + line
 
-    # Say
-    elif re.match(r'\[(\d+):(\d+):(\d+)\]: \[Say\] \(.+\) (.+): (.+)$', line):
-        return ":speech_balloon: :speech_balloon: " + line
+    elif "tells you" in line or "You told" in line:
+        line = re.sub("You told", "You tell", line)
+        return ":busts_in_silhouette:" + line
 
-    # Whisper
-    elif re.match(r'\[(\d+):(\d+):(\d+)\]: \[Whisper\] \(.+\) (.+): (.+)$', line):
-        return ":envelope: :envelope: " + line
-
-    # Death
-    elif re.match(r'\[(\d+):(\d+):(\d+)\]: \[Death Announcement\] (.+) was killed by (.+)\.', line):
-        return "<:dead:290486234957217792> <:dead:290486234957217792> " + line
-
-    # Resurrect
-    elif re.match(r'\[(\d+):(\d+):(\d+)\]: \[Resurrect Announcement\] (.+) was resurrected by (.+)\.$', line):
-        return ":heart: :heart: " + line
-
-    # Skin
-    elif re.match(r'\[(\d+):(\d+):(\d+)\]: \[Skin Announcement\] (.+)$', line):
-        return "<:gift:290486235137572864> <:gift:290486235137572864> " + line
-
-    # Roll
-    elif re.match(r'\[(\d+):(\d+):(\d+)\]: \[Roll Announcement\] \(.+\) (.+) (.+)$', line):
-        return ":game_die: :game_die: " + line
-
-    # Vote
-    elif re.match(r'\[(\d+):(\d+):(\d+)\]: \[Vote Announcement\] (.+)$', line):
-        return ":white_check_mark: :white_check_mark: " + line
-
-    # Kick
-    elif re.match(r'\[(\d+):(\d+):(\d+)\]: \[Kick Announcement\] (.+)$', line):
-        return ":no_pedestrians: :no_pedestrians: " + line
-
-    # Announcement
-    elif re.match(r'\[(\d+):(\d+):(\d+)\]: \[Announcement\] (.+)$', line):
-        return "" + line
+    elif "auctions" in line or "You auction" in line:
+        return ":moneybag:" + line
 
     # No match found
     else:
-        return "" + line
+        return None
 
 def DontStarveReactionFilter(line):
     line = line.lower()
 
-    # Deerclops
-    if "deerclop" in line:
-        return "deerclops:317613422252523521"
-
     # Moose Goose
-    elif "moose" in line or "goose" in line:
-        return "moose:317613438945853440"
+    if "vindi" in line or "vindicator" in line:
+        return ":sword:"
 
     # Mosling
-    elif "mosling" in line:
-        return "mosling:317613438618697728"
-
-    # Bearger
-    elif "bear" in line:
-        return "bearger:317613422084620300"
-
-    # Dragonfly
-    elif "dragonfly" in line or "dfly" in line:
-        return "dragonfly:317613422403387392"
-
-    # Spider Queen
-    elif "spider queen" in line:
-        return "spider_queen:317613590406234114"
-
-    # Ancient Guardian
-    elif "guardian" in line:
-        return "guardian:317613550052966400"
-
-    # Klaus
-    elif "klaus" in line:
-        return "klaus:317613506755166210"
-
-    # Bee Queen
-    elif "bee queen" in line:
-        return "bee_queen:317613507128328193"
-
-    # Toadstool
-    elif "toad" in line:
-        return "toadstool:317613507552083968"
-
-    # Ancient Fuelweaver
-    elif "fuelweaver" in line:
-        return "fuelweaver:317619375366733824"
+    elif "idksomething" in line:
+        return ":dagger:"
 
     # No match found
     else:
@@ -119,7 +56,7 @@ async def file_tail(channelID, filename, wait):
     await client.wait_until_ready()
     channel = client.get_channel(id=channelID)
 
-    await channel.send("--This channel will be posted to--")
+    await channel.send("Relaying in-game messages here...")
 
     try:
         file = open(filename, 'r', encoding='utf-8')
@@ -138,12 +75,39 @@ async def file_tail(channelID, filename, wait):
         except UnicodeDecodeError:
             print("Encountered unknown character in server log, skipping lines.")
         else:
-            for line in lines:    # Not EOF
-                print(line)
+            for line in lines:
+
+                current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+                line = re.sub('^\[.*?\] ', '', line)
+                line = re.sub('\, \'', ': ``', line)
+                line = re.sub('\'$', '``', line)
+
+                if "Players on EverQuest" in line or "Players in EverQuest" in line:
+                    whoList = f":scroll: Taking attendance at `{current_time}` ```"
+
+                    for line in lines:
+                        # line = re.sub('^\[.*?\] ', '', line)
+                        line = re.sub(' ZONE.*$', '', line)
+                        line = re.sub(" AFK ", "", line)
+                        line = re.sub(" LFG", "", line)
+
+                        if ("<" in line and ">" in line) or "ANONYMOUS" in line:
+                            whoList = whoList + line
+
+                        elif "There is " in line or "There are " in line or "Your who request was cut short" in line:
+                            whoList = whoList + "```\n"
+                            message = await channel.send(whoList)
+                            break
+
                 message_line = DontStarvePrependEmoji(line)
+
+                if message_line is None:
+                    continue
 
                 try:
                     message = await channel.send(message_line)
+
                 except discord.Forbidden:
                     print("FORBIDDEN EXCEPTION (403): Bot doesn't have permissions to send message.")
                 except discord.NotFound:
@@ -155,9 +119,12 @@ async def file_tail(channelID, filename, wait):
                 except discord.InvalidArgument:
                     print("INVALID ARGUMENT EXCEPTION: Destination parameter invalid, couldn't send message.")
                 else:
+
                     reaction = DontStarveReactionFilter(line)
+
                     if reaction is None:
                         continue
+
                     skiptime = 1.0
                     await asyncio.sleep(skiptime)
                     skipped += skiptime
